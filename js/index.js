c3273eac9996febbef989f9a0923c74f816a41ef
anna.kolintso <anna.kolintso@gmail.com>
2020-01-09 15:55:38 +0200

    add film

---
+++ js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+let favouriteFilm = {};
+favouriteFilm.name = "Maleficent";
+favouriteFilm.year = 2019;
+favouriteFilm.country = "USA";
+favouriteFilm.actor = {
+    name: "Angelina",
+    surname: "Joly"
+};
+favouriteFilm.timeLine = "119 min";
+favouriteFilm.genre = "fantasy";
+favouriteFilm.isWorthWatch = true;
+let question = confirm(`Have you watched ${favouriteFilm.name}?`);
+if (question) {
+    favouriteFilm.isWorthWatch = true;
+} else {
+    favouriteFilm.isWorthWatch = false;
+}
+
+
+alert("It worth watching");
