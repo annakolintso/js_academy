df9d4ec6d8b5689ad71f3ebbe8ad5fa383a729b6
anna.kolintso <anna.kolintso@gmail.com>
2020-01-14 16:53:20 +0200

    add switch

--- js/index.js
+++ js/index.js
@@ -20,44 +20,83 @@ if (question) {
     favouriteFilm.isWorthWatch = false;
     alert("It worth watching");
 }
+
 /////////
 function changeRating(rating) {
     rating = Number(rating);
-    if (rating >= 10){
+    if (rating >= 10) {
         favouriteFilm.rating = 10;
-    } else if (rating <= 0){
+    } else if (rating <= 0) {
         favouriteFilm.rating = 0;
     } else {
         favouriteFilm.rating = rating;
     }
 }
+
 let newRating = prompt(`What rating for ${favouriteFilm.name}?`);
 changeRating(newRating);
+
 ////////
-function getRecommendation(rating){
+// function getRecommendation(rating) {
+//     rating = Number(rating);
+//     if (rating <= 0) {
+//         alert("Awful film");
+//     } else if (rating == 1) {
+//         alert("The worst film");
+//     } else if (rating == 2) {
+//         alert("Very bad film");
+//     } else if (rating == 3) {
+//         alert("Bad film");
+//     } else if (rating == 4) {
+//         alert("Not interesting film");
+//     } else if (rating == 5) {
+//         alert("Good film");
+//     } else if (rating == 6) {
+//         alert("Very good film");
+//     } else if (rating == 7) {
+//         alert("Interesting film");
+//     } else if (rating == 8) {
+//         alert("The best film");
+//     } else if (rating == 9) {
+//         alert("It worth watching");
+//     } else if (rating >= 10) {
+//         alert("Awesome film");
+//     } else {
+//         alert("You use incorrect answer");
+//     }
+// }
+//
+// getRecommendation(newRating);
+
+///////
+function getRecommendation(rating) {
     rating = Number(rating);
-    if (rating <= 0){
-        alert("Awful film");
-    } else if (rating == 1){
-        alert("The worst film");
-    } else if (rating == 2){
-        alert("Very bad film");
-    } else if (rating == 3){
-        alert("Bad film");
-    } else if (rating == 4){
-        alert("Not interesting film");
-    } else if (rating == 5){
-        alert("Good film");
-    } else if (rating == 6){
-        alert("Very good film");
-    } else if (rating == 7){
-        alert("Interesting film");
-    } else if (rating == 8){
-        alert("The best film");
-    } else if (rating == 9){
-        alert("It worth watching");
-    } else if (rating >= 10) {
-        alert("Awesome film");
+    switch (rating) {
+        case 0:
+            alert("Awful film");
+        case 1:
+            alert("The worst film");
+        case 2:
+            alert("Very bad film");
+        case 3:
+            alert("Bad film");
+        case 4:
+            alert("Not interesting film");
+        case 5:
+            alert("Good film");
+        case 6:
+            alert("Very good film");
+        case 7:
+            alert("Interesting film");
+        case 8:
+            alert("The best film");
+        case 9:
+            alert("It worth watching");
+        case 10:
+            alert("Awesome film");
+        default:
+            alert("You use incorrect answer");
     }
 }
-getRecommendation(newRating);
+
+ getRecommendation(newRating);
