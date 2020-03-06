481c7bf632932d7cd6143f0132454af3c430179b
anna.kolintso <anna.kolintso@gmail.com>
2020-03-06 18:58:42 +0200

    add local storage

--- js/promise.js
+++ js/promise.js
@@ -1,3 +1,7 @@
+let postData = {
+    posts: [],
+};
+
 function xhr (method, url, body = null){
     return new Promise((resolve,reject) => {
         let xhr = new XMLHttpRequest();
@@ -11,33 +15,54 @@ function xhr (method, url, body = null){
 }
 
 function postLoad(response = []) {
+    let promiseArr = [];
     for(let i = 0; i < 10; i++){
         let random = getRandomInt(99);
         let post = response[random];
-        let p = document.createElement('p');
-        let ul = document.createElement('ul');
-        let div = document.createElement('div');
-        p.innerHTML = post.title;
-        div.append(p);
-        div.append(ul);
-        xhr("GET", `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
-            .then(res => commentLoad(res, div))
-    }
+        let promise = xhr("GET", `https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
+            .then(res => {
+                post['comments'] = res;
+                postData.posts.push(post);
+            });
+        promiseArr.push(promise);
+    };
+    Promise.all(promiseArr)
+        .then(()=>{
+            let postDataJson = JSON.stringify(postData);
+            window.localStorage.setItem('posts', postDataJson);
+            createPosts(postData);
+        })
 };
-
-let commentLoad = (comments, post) => {
-    let list = post.querySelector('ul');
-    for (let i=0; i<comments.length; i++){
-        let item = document.createElement('li');
-        item.innerHTML = `${comments[i].name} + ${comments[i].email} + ${comments[i].body}`;
-        list.append(item);
-    }
-    document.body.append(post);
-}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-xhr('GET', 'https://jsonplaceholder.typicode.com/posts')
-    .then(res => postLoad(res));
+function createPosts(data) {
+    let div = document.createElement('div');
+    data.posts.forEach(post => {
+        let p = document.createElement('p');
+        let ul = document.createElement('ul');
+        p.innerHTML = post.title;
+        post.comments.forEach(comment => {
+            let item = document.createElement('li');
+            item.innerHTML = `${comment.name} + ${comment.email} + ${comment.body}`;
+            ul.append(item);
+        })
+        div.append(p);
+        div.append(ul);
+    })
+    document.body.append(div)
+}
+
+function getLocalStorage() {
+    let postLocalData = JSON.parse(window.localStorage.getItem('posts'));
+    if (postLocalData === null){
+        xhr('GET', 'https://jsonplaceholder.typicode.com/posts')
+            .then(res => postLoad(res));
+    }else{
+        createPosts(postLocalData)
+    }
+}
+
+getLocalStorage();
